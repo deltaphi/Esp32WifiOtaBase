7405a857156bf940a61b30456634d02faad0867e
Damian Philipp <damian.philipp@gmx.net>
2021-02-25 19:51:07 +0100

    Add ArduinoOTA capabilities

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 
 #include <SPIFFS.h>
 
+#include <ArduinoOTA.h>
+#include <ESPmDNS.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
 #include "Settings.h"
 #include "wifiManager.h"
 
@@ -24,6 +29,44 @@ void setup() {
 
   setupWifi();
   startWifi(settings.getSsid(), settings.getPassword());
+
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Wifi Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  ArduinoOTA
+      .onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
+          type = "sketch";
+        else  // U_SPIFFS
+          type = "filesystem";
+
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+        SPIFFS.end();
+        Serial.println("Start updating " + type);
+      })
+      .onEnd([]() { Serial.println("\nEnd"); })
+      .onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+      })
+      .onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR)
+          Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR)
+          Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR)
+          Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR)
+          Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR)
+          Serial.println("End Failed");
+      });
+
+  ArduinoOTA.begin();
 }
 
 void loop() {
@@ -32,4 +75,6 @@ void loop() {
   } else {
     digitalWrite(kLedPin, LOW);
   }
+
+  ArduinoOTA.handle();
 }
